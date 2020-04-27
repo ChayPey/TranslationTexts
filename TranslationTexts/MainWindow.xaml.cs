677d46f1ad8bd10f3f0f64a21c1c830f97cef460
Tretyk Vasiliy <chaylord@bk.ru>
2020-04-27 15:16:49 +0500

    Non1

---
+++ TranslationTexts/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TranslatorAPI;
+
+namespace TranslationTexts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Languages languages;
+        private Translator translator;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string APIKey = "trnsl.1.1.20200421T094203Z.8f9595c509cb9fc9.0b95698bf5cc031b04cc33a1099595b862c1af1a";
+            translator = new Translator(APIKey);
+            languages = translator.SupportedLanguages();
+            languages.langs = languages.langs.ToDictionary(x => x.Value, x => x.Key);
+
+            foreach (string p in languages.langs.Keys)
+            {
+                ComboBoxText.Items.Add(p);
+                ComboBoxTranslation.Items.Add(p);
+            }
+            ComboBoxText.Text = "Русский";
+            ComboBoxTranslation.Text = "Английский";
+            //TextBoxTranslation.Text = translator.Translate(TextBoxText.Text, languages.langs[ComboBoxText.Text], languages.langs[ComboBoxTranslation.Text]).text[0];
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxTranslation.Text = translator.Translate(TextBoxText.Text, languages.langs[ComboBoxText.Text], languages.langs[ComboBoxTranslation.Text]).text[0];
+        }
+
+        private void swap_text_Click(object sender, RoutedEventArgs e)
+        {
+            string buffer = TextBoxTranslation.Text;
+            TextBoxTranslation.Text = TextBoxText.Text;
+            TextBoxText.Text = buffer;
+            buffer = ComboBoxText.Text;
+            ComboBoxText.Text = ComboBoxTranslation.Text;
+            ComboBoxTranslation.Text = buffer;
+
+        }
+    }
+}
---
+++ TranslatorAPI/Translator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TranslatorAPI
+{
+    public class Translator
+    {
+        private string APIKey = "trnsl.1.1.20200421T094203Z.8f9595c509cb9fc9.0b95698bf5cc031b04cc33a1099595b862c1af1a";
+        public Translator(string APIKey)
+        {
+            this.APIKey = APIKey;
+        }
+        public Translator()
+        {
+        }
+        public Languages SupportedLanguages()
+        {
+            // Получить список поддерживаемых языков
+            // Создание текста запроса
+            string language = $"ru";
+            string dataRequest = $"https://translate.yandex.net/api/v1.5/tr.json/getLangs?ui={language}&key={APIKey}";
+
+            // Отправка запроса к Web API
+            WebRequest request = WebRequest.Create(dataRequest);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            WebResponse response = request.GetResponse();
+
+            // Статус запроса: удачен, неудачен
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            if (((HttpWebResponse)response).StatusDescription != "OK")
+            {
+                return null;
+            }
+
+            // Чтение запроса: распаковка JSON
+            string dataJson;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                dataJson = reader.ReadToEnd();
+            }
+            Languages languages = JsonSerializer.Deserialize<Languages>(dataJson);
+
+            // Закрытие соединений
+            response.Close();
+            return languages;
+        }
+        public Translation Translate(string text, string lang, string toLang)
+        {
+            // Создание текста запроса
+            string dataRequest = $"https://translate.yandex.net/api/v1.5/tr.json/translate?lang={lang}-{toLang}&key={APIKey}&text={text}";
+
+            // Отправка запроса к Web API
+            WebRequest request = WebRequest.Create(dataRequest);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            WebResponse response = request.GetResponse();
+
+            // Статус запроса: удачен, неудачен
+            if (((HttpWebResponse)response).StatusDescription != "OK")
+            {
+                return null;
+            }
+
+            // Чтение запроса: распаковка JSON
+            string dataJson;
+            using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+            {
+                dataJson = reader.ReadToEnd();
+            }
+            Translation translation = JsonSerializer.Deserialize<Translation>(dataJson);
+
+            // Закрытие соединений
+            response.Close();
+            return translation;
+        }
+    }
+
+    public class Languages
+    {
+        public string[] dirs { get; set; }
+        public Dictionary<string, string> langs { get; set; }
+    }
+
+    public class Translation
+    {
+        public int code { get; set; }
+        public string lang { get; set; }
+        public string[] text { get; set; }
+    }
+}
